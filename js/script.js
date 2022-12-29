fc5d9b1d95685a0f812c299d638deb45f39ee42d
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-12-29 23:20:40 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Programmer", "Front-End Developer", "Photographer", "Video Editor", "Programmer"],
+    typeSpeed:70,
+    BackSpeed:60,
+    loop:true
+})
